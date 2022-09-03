30e1b010c3d5ff017d1237ab0a3222ba3ad67c78
Mohit gupta <mohit@mogify.com>
2022-09-03 04:52:16 +0000

    init

--- deploy.sh
+++ deploy.sh
@@ -15,35 +15,28 @@ fi
 
 
 echo "1- Start to push on registry the redis docker image which can be used as master or slave in the stack..."
-docker-compose -f agile-redis-master-slave/agile-redis-Dockercompose.yml build
-docker-compose -f agile-redis-master-slave/agile-redis-Dockercompose.yml push
+docker compose -f agile-redis-master-slave/agile-redis-Dockercompose.yml build
+docker compose -f agile-redis-master-slave/agile-redis-Dockercompose.yml push
 echo "(1)End to build and push redis image to registry."
 echo "-------------------------------------------------------\n"
 
 echo "2- Start to push on registry the redis docker image which will be used to build sentinel..."
-docker-compose -f agile-redis-sentinel/agile-redis-sentinel-Dockercompose.yml build
-docker-compose -f agile-redis-sentinel/agile-redis-sentinel-Dockercompose.yml push
+docker compose -f agile-redis-sentinel/agile-redis-sentinel-Dockercompose.yml build
+docker compose -f agile-redis-sentinel/agile-redis-sentinel-Dockercompose.yml push
 echo "(2)End to build and push redis sentinel image to registry."
 echo "-------------------------------------------------------\n"
 
 
-echo "3- Start to push our python app example on the registry..."
-docker-compose -f python-app-example/compose-app.yml build
-docker-compose -f python-app-example/compose-app.yml push
-echo "(3)End to push our python app example on the registry."
-echo "-------------------------------------------------------\n"
-
-
 echo "4- Start to deploy the stack..."
-export SENTINEL_IP=`docker node inspect --format {{.Status.Addr}} $SENTINEL_HOSTNAME`
-export REDIS_MASTER_IP=`docker node inspect --format {{.Status.Addr}} $REDIS_MASTER_HOSTNAME`
+export SENTINEL_IP=$SENTINEL_HOSTNAME
+export REDIS_MASTER_IP=$REDIS_MASTER_HOSTNAME
 
 echo "Sentinel hostname and IP: $SENTINEL_HOSTNAME -  $SENTINEL_IP"
 echo "Redis Master hostname and IP: $REDIS_MASTER_HOSTNAME - $REDIS_MASTER_IP"
 echo "Redis slave 1 hostname: $REDIS_SLAVE_NODE1_HOSTNAME"
 echo "Redis slave 2 hostname: $REDIS_SLAVE_NODE2_HOSTNAME"
 
-docker stack deploy -c agile-redis-stack.yml stackredis
+docker stack deploy --with-registry-auth -c agile-redis-stack.yml stackredis
 printf "(4)End to deploy the stack... Please wait until the services started\n\n\n"
 
 sleep 3s
