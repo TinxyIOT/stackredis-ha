d933c9510494ec1309f46dbd29c24a54da36342e
Mohit Gupta <mohit@mogify.com>
2022-09-04 17:26:38 +0530

    aa

--- agile-redis-master-slave/agile-redis-entrypoint.sh
+++ agile-redis-master-slave/agile-redis-entrypoint.sh
@@ -5,14 +5,15 @@ CONF_FILE=/etc/redis/redis.conf
 if [ ! -f $CONF_FILE ]
 then
     echo "appendonly yes" > $CONF_FILE
-    echo "replica-announce-ip ${REDIS_SLAVE_HOST}" >> $CONF_FILE
     echo "masterauth McjN2UhLrwrDKf" >> $CONF_FILE   
     echo "requirepass McjN2UhLrwrDKf" >> $CONF_FILE   
 
     if [ -n "$REDIS_MASTER_HOST" ]
     then
         echo "slaveof redis-master 6379" > $CONF_FILE
-    # else 
+        echo "replica-announce-ip ${REDIS_SLAVE_HOST}" >> $CONF_FILE
+    else 
+        echo "replica-announce-ip ${REDIS_MASTER_HOST}" >> $CONF_FILE
     fi
     chown redis:redis $CONF_FILE
 fi
